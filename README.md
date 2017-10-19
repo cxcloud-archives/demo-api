ec5558af984f436759653ebe68dc4266eaf98590
Sallar Kaboli <sallar.kaboli@gmail.com>
2017-10-19 14:22:49 +0300

    Add @cxcloud/facade module

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# cxcloud-demo-facade
+# CXCloud Facade Demo
+
+This repository uses [CXCloud Facade](https://github.com/cxcloud/cxcloud-facade) module and shows off it's capabilities. You can use this as a starting point for your CXCloud projects.
--- src/v1.ts
+++ src/v1.ts
@@ -1,5 +1,8 @@
 import { Router } from 'express';
+import { Categories } from '@cxcloud/facade';
 
 export const router = Router();
 
-router.get('/hello', (req, res) => res.json('hello'));
+router.get('/categories', (req, res) => {
+  Categories.fetchAll().then(cats => res.json(cats));
+});
