ae115bfae686454fc4fe0da418f9f29e1102b7d8
Sallar Kaboli <sallar.kaboli@gmail.com>
2017-11-07 13:21:20 +0200

    Add orders controller

--- src/controllers/all.ts
+++ src/controllers/all.ts
@@ -1,4 +1,5 @@
 export * from './carts-controller';
+export * from './orders-controller';
 export * from './products-controller';
 export * from './categories-controller';
 export * from './auth-controller';
