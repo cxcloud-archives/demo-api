3bb60e9d80e0d83731f84eeee335469177927d2b
Sallar Kaboli <sallar.kaboli@gmail.com>
2017-11-07 13:01:13 +0200

    Add product endpoints

--- src/controllers/products-controller.ts
+++ src/controllers/products-controller.ts
@@ -1,19 +1,23 @@
 import { GET, Path, PathParam } from 'typescript-rest';
 import { Products } from '@cxcloud/facade/dist/commerce';
-import { Product } from '@cxcloud/facade/dist/sdk/types/products';
+import {
+  Product,
+  PaginatedProductResult
+} from '@cxcloud/facade/dist/sdk/types/products';
 
-/**
- * This is a demo operation to show how to use typescript-rest library.
- */
 @Path('/products')
 export class ProductsController {
-  /**
-     * Send a greeting message.
-     * @param name The name that will receive our greeting message
-     */
   @Path(':id')
   @GET
-  async getProduct(@PathParam('id') id: string): Promise<Product> {
-    return await Products.findById(id);
+  getProduct(@PathParam('id') id: string): Promise<Product> {
+    return Products.findById(id);
+  }
+
+  @Path('/byCategory/:categoryId')
+  @GET
+  getProductsByCategory(
+    @PathParam('categoryId') categoryId: string
+  ): Promise<PaginatedProductResult> {
+    return Products.findByCategoryId(categoryId);
   }
 }
