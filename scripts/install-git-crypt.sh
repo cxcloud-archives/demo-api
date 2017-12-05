0448f737d5d93d050ca717d475a11bcc730156e0
Sallar Kaboli <sallar.kaboli@gmail.com>
2017-12-05 15:59:50 +0200

    Remove sudo

--- scripts/install-git-crypt.sh
+++ scripts/install-git-crypt.sh
@@ -7,7 +7,7 @@ curl -Lo git-crypt.zip https://github.com/AGWA/git-crypt/archive/master.zip
 unzip git-crypt.zip
 cd git-crypt-master
 make
-sudo install git-crypt /usr/local/bin
+install git-crypt /usr/local/bin
 cd $TRAVIS_BUILD_DIR
 openssl aes-256-cbc -K $encrypted_bb5bb77d3466_key -iv $encrypted_bb5bb77d3466_iv -in git-crypt.key.enc -out git-crypt.key -d
 git-crypt unlock git-crypt.key
